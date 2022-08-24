347c1d005d14b9f3e3c91e20583f7f1853ed16ee
ElianeO <elianecristina_d@yahoo.com>
2022-08-24 10:35:55 -0300

    24-08  (Revisão de JS)

--- 12-reduce.js
+++ 12-reduce.js
@@ -2,8 +2,8 @@ let nums = [2,3,5,7,11,13,17,19,23,29,31]
 let nomes = ['Aderbal', 'Joeliton', 'Neurivânia', 'Terebintina', 'Leucimara']
 
 //reduce() ára somar todos os elementos do vetor nums
-let soma = nums.reduce((elemento, acumulator) => acumulator + elemento)
+let soma = nums.reduce((acumulator, elemento) => acumulator + elemento)
 
-let listaNomes = nomes.reduce((el, acum) => el.toUpperCase() + ', ' + acum.toUpperCase())
+let listaNomes = nomes.reduce((acum, el) => acum.toUpperCase() + ', ' + el.toUpperCase())
 
 console.log({soma, listaNomes})
